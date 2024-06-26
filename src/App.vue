fc9b0cc92430c02ccbd64495be7970131a07c2df
RB <RBercasio22@gmail.com>
2024-06-26 16:03:39 -0700

    First commit

--- src/App.vue
+++ src/App.vue
@@ -1,20 +1,49 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div class="main-nav">
+    <div class="left-side-bar">
+      <p class="p-menu">Fixedsize@300 by 100px</p>
+    </div>
+    <div class="center-bar">
+      <p class="p-menu">Flex: 1, </p>
+    </div>
+    <div class="right-side-bar">
+      <p class="p-menu">Fixedsize@300 by 100px</p>
+    </div>
+  </div>
+
+  <div class="main-content-parent">
+      <Main />
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci dolorem nesciunt et eveniet cumque consequatur, maiores libero minus sapiente enim! Commodi, quos sint, reiciendis est modi dicta debitis porro cupiditate doloribus nesciunt beatae asperiores delectus repellat ea laborum dolore mollitia.</p>
+
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Voluptates ratione culpa modi nobis aliquid, accusamus tenetur labore consectetur reiciendis corrupti officia eum in! Aperiam aut reiciendis similique impedit illum nisi, veniam molestiae saepe vel cupiditate adipisci ducimus nam nulla obcaecati sint voluptatibus neque, voluptatem odit est alias facere iusto sit?</p>
+
+      <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ipsa magni delectus dolorem debitis voluptas fugit! Veritatis voluptatum labore atque alias tempora perferendis omnis modi doloremque asperiores. Reiciendis atque unde libero quibusdam nostrum, nisi deleniti ducimus aliquid suscipit nobis, eum omnis impedit dicta nam beatae laborum quisquam, enim quod optio animi!</p>
+  </div>
+ 
+  <div class="footer">
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Ullam aut, atque, ea soluta aperiam quibusdam obcaecati dignissimos tenetur commodi repudiandae itaque dolor enim mollitia, velit temporibus maxime eos eligendi laboriosam?</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Magnam iusto explicabo nisi omnis, quibusdam cum temporibus. Iste aperiam necessitatibus eius temporibus doloribus. Corporis, a? Enim, repellat ad nam sequi minus distinctio mollitia nemo obcaecati ea, delectus facere beatae eius aliquid?</p>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Nesciunt suscipit quae, voluptates excepturi ratione iusto veritatis illo voluptatum officia ipsum dolorum optio asperiores eos laborum, facere doloremque vel delectus tenetur facilis. Quos, similique quas! Nobis eligendi tempore voluptatum modi perspiciatis?</p>
+</div>
+
+
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+//import HelloWorld from './components/HelloWorld.vue'
+import Main from './components/Main.vue'
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    //HelloWorld,
+    Main
   }
 }
 </script>
 
 <style>
+/*
 #app {
   font-family: Avenir, Helvetica, Arial, sans-serif;
   -webkit-font-smoothing: antialiased;
@@ -22,5 +51,127 @@ export default {
   text-align: center;
   color: #2c3e50;
   margin-top: 60px;
+  
 }
+*/
+* {
+  padding: 0;
+  margin: 0;
+  background-color: #1a0517;
+  color: white;
+  box-sizing: border-box;
+}
+
+.main-nav {
+  top: 0;
+  left: 0;
+  display: flex;
+  flex-direction: row;
+  width: 100%;
+  height: 100px;
+  border-bottom: 1px solid gray;
+  position: fixed;
+  justify-content: space-between;
+  align-items: center;
+  margin-bottom: 100px;
+  z-index: 1;
+}
+
+.p-menu {
+  color: white;
+  font-size: 16px;
+  letter-spacing: 3px;
+  font-weight: bold;
+  text-transform: uppercase;
+}
+
+.left-side-bar {
+  background-color: transparent;
+  display: flex;
+  width: 300px;
+  height: 70px;
+  justify-content: flex-start;
+  align-items: center;
+  flex-direction: row;
+  border: 4px solid green;
+  margin-right: 10px;
+
+}
+
+.left-side-icon{
+  position: fixed;
+  top: 30%;
+  bottom: 50%;
+  left: 0;
+  transform: translateY(50%);
+  background-color: white;
+
+}
+
+.center-bar {
+  background-color: transparent;
+  display: flex;
+  height: 70px;
+  justify-content: center;
+  align-items: center;
+  flex-direction: row;
+  border: 4px solid green;
+  flex: 1;
+
+}
+.right-side-bar {
+  background-color: transparent;
+  display: flex;
+  width: 300px;
+  height: 70px;
+  justify-content: flex-end;
+  align-items: center;
+  flex-direction: row;
+  border: 4px solid green;
+  margin-left: 10px;
+
+}
+
+.main-content-parent {
+  margin-bottom: 50px;
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  padding: 10px;
+  text-align: center
+
+}
+.footer {
+  border-top: 1px solid gray;
+  display: flex;
+  flex-direction: row; /* Change to column for stacking items */
+   /* Center items vertically */
+  align-items: center; /* Center items horizontally */
+  padding: 30px;
+  height: 500px;
+  text-align: center;
+  
+}
+
+p {
+
+  margin: 10px;
+  border-radius: 10px;
+  text-transform: uppercase;
+  letter-spacing: 3px;
+  font-size: 12px;
+  color: white;
+}
+
+
+@media (max-width: 500px) {
+  .footer {
+    display: flex;
+    flex-direction: column;
+    margin-bottom: 10px;
+    
+  }
+}
+
+
 </style>
---
+++ src/components/Main.vue
@@ -0,0 +1,85 @@
+<template>
+    <div class="main-content-1">
+      <div class="left-side-icon">
+
+          <p>Facebook</p>
+          <p>Linkedin</p>
+          <p>Github</p>
+
+      </div>
+        <p class="p-testing">Lorem ipsum dolor sit amet consectetur adipisicing elit. Adipisci aliquid hic, quia dolorum quasi enim sequi cupiditate molestias magnam cum quisquam deserunt minus illum aperiam ut autem consequatur harum rem praesentium vero. Illum ipsum animi, ipsa, deleniti dignissimos esse tempora placeat culpa dolorem veritatis iusto eum dicta atque, perferendis accusantium odio? Illum nemo nostrum corporis accusantium totam ad a et officia, iure, saepe facere non cumque similique itaque aliquid, eius nesciunt placeat vero. Omnis voluptatibus eius inventore ex cum exercitationem hic saepe blanditiis corporis maiores, repellat, ipsa corrupti officia sint incidunt possimus! Iusto aperiam modi dolorum, cupiditate beatae dolore in!</p>
+        <p class="p-testing">Lorem ipsum dolor sit amet consectetur adipisicing elit. Quos ad velit commodi sapiente officia? Assumenda, molestiae consectetur et aut tempore voluptatum fugit libero in eveniet officia deleniti esse, voluptatem perferendis officiis? Dolores aliquid pariatur nemo et, tempore dolorem vel architecto minima, natus, ipsa consequatur. Temporibus cumque nulla ratione eligendi repellat assumenda eos, commodi, maiores praesentium necessitatibus voluptates culpa perspiciatis? Veniam adipisci officia error laudantium architecto, cumque ullam eligendi reiciendis tenetur blanditiis dolorum quisquam impedit nam debitis! Necessitatibus praesentium quisquam magni error explicabo iure esse commodi est unde, aut voluptatem laborum, dolores nisi in non fugiat voluptate voluptates harum. Placeat, fugit.</p>
+        <p class="p-testing">Lorem ipsum dolor, sit amet consectetur adipisicing elit. Corporis odio quasi doloribus possimus distinctio atque nam laboriosam sint a necessitatibus, sit dignissimos repellendus molestiae veniam ducimus blanditiis ea voluptatum, tempora quisquam perspiciatis sapiente eaque. Hic mollitia quo nesciunt excepturi nulla fuga saepe? Odio asperiores soluta laboriosam modi fuga voluptate ex quam autem optio odit deleniti placeat in quaerat numquam animi quod quidem eaque officiis cupiditate explicabo, beatae, deserunt perspiciatis hic? Amet recusandae error nisi ratione, nulla in labore expedita corrupti sed rem commodi, itaque dolorem quia! Ab molestias, commodi obcaecati quisquam, dolore nulla sunt, ut numquam amet deleniti facilis hic?</p>
+       
+       
+    </div>
+
+    <div class="main-content-2">
+      <p class="p-testing">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt nihil esse vel ipsa, ullam voluptatum eligendi minus vitae temporibus ducimus tenetur reiciendis ad iste laboriosam totam nesciunt! Repellendus corrupti officia error officiis soluta aperiam vero porro aspernatur? Officia ad molestias expedita dicta earum culpa eius magnam doloremque sed cumque ab doloribus maxime impedit atque ipsa, a ipsam. Eum vel aliquam quo ullam dolor laboriosam et nihil at, sint natus quisquam sed reiciendis? Fugiat assumenda officia quae quam recusandae tempora id, dolorum cupiditate nam facilis molestiae doloremque suscipit, vel atque iusto totam tenetur laboriosam quibusdam eos maxime consequatur fugit. Commodi, quos.</p>
+      <p class="p-testing">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt nihil esse vel ipsa, ullam voluptatum eligendi minus vitae temporibus ducimus tenetur reiciendis ad iste laboriosam totam nesciunt! Repellendus corrupti officia error officiis soluta aperiam vero porro aspernatur? Officia ad molestias expedita dicta earum culpa eius magnam doloremque sed cumque ab doloribus maxime impedit atque ipsa, a ipsam. Eum vel aliquam quo ullam dolor laboriosam et nihil at, sint natus quisquam sed reiciendis? Fugiat assumenda officia quae quam recusandae tempora id, dolorum cupiditate nam facilis molestiae doloremque suscipit, vel atque iusto totam tenetur laboriosam quibusdam eos maxime consequatur fugit. Commodi, quos.</p>
+      <p class="p-testing">Lorem ipsum dolor sit amet consectetur adipisicing elit. Sunt nihil esse vel ipsa, ullam voluptatum eligendi minus vitae temporibus ducimus tenetur reiciendis ad iste laboriosam totam nesciunt! Repellendus corrupti officia error officiis soluta aperiam vero porro aspernatur? Officia ad molestias expedita dicta earum culpa eius magnam doloremque sed cumque ab doloribus maxime impedit atque ipsa, a ipsam. Eum vel aliquam quo ullam dolor laboriosam et nihil at, sint natus quisquam sed reiciendis? Fugiat assumenda officia quae quam recusandae tempora id, dolorum cupiditate nam facilis molestiae doloremque suscipit, vel atque iusto totam tenetur laboriosam quibusdam eos maxime consequatur fugit. Commodi, quos.</p>
+    </div>
+
+
+</template>
+
+
+
+<script>
+
+export default {
+  name: 'Main',
+  
+}
+
+
+</script>
+
+
+
+<style scoped>
+
+
+.p-testing {
+    color: gray;
+    text-transform: uppercase;
+    font-size: 12px;
+    letter-spacing: 2px;
+    border: 1px solid white;
+    padding: 10px;
+    margin-top: 10px;
+    border-radius: 10px;
+}
+
+
+.main-content-1 {
+  margin-top: 100px;
+  margin-bottom: 10px;
+  display: flex;
+  flex-direction: row;
+  padding: 10px;
+  text-align: center;
+  
+}
+.main-content-2 {
+
+ 
+  display: flex;
+  flex-direction: row;
+  padding: 10px;
+  text-align: center;
+  
+}
+
+@media (max-width: 500px) {
+  .p-testing,
+  .main-content {
+    display: flex;
+    flex-direction: column;
+  }
+  
+}
+
+
+
+</style>
