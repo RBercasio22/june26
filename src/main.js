1138a53a6d4c89153f87e43413d890215d940d31
RB <53470171+RBercasio22@users.noreply.github.com>
2024-07-07 20:38:49 +0800

    Update main.js

--- src/main.js
+++ src/main.js
@@ -4,3 +4,35 @@ import App from './App.vue'
 createApp(App).mount('#app')
 
 //https://snipboard.io/ZKyNrH.jpg
+
+
+/*
+
+<template>
+  <div>
+    <h1>{{ msg }}</h1>
+    <input v-model="todo" />
+    <button @click="addTodo">Add</button>
+    <ul>
+      <li v-for="(item, index) in list" :key="index">{{ item }}</li>
+    </ul>
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+
+const msg = ref('TODO LIST')
+const list = ref([])
+const todo = ref('')
+
+function addTodo() {
+  if (todo.value.trim() !== '') {
+    list.value.push(todo.value)
+    todo.value = ''
+  }
+}
+
+</script>
+
+*/
